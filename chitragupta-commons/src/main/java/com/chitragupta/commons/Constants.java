c0baf7dcae9ba13cb7a36b0e50f5d4a0f40380de
abhay447 <abhay447@gmail.com>
2023-06-04 13:41:27 +0530

    added Constants.java

--- chitragupta-commons/src/main/java/com/chitragupta/commons/Constants.java
+++ chitragupta-commons/src/main/java/com/chitragupta/commons/Constants.java
@@ -2,8 +2,8 @@ package com.chitragupta.commons;
 
 public class Constants {
 
-    public static final String RAW_EVENT_TOPIC = "raw_events";
+    public static final String RAW_EVENT_TOPIC = "RAW_EVENTS";
 
-    public static final String ENRICHED_EVENT_TOPIC = "enriched_events";
+    public static final String ENRICHED_EVENT_TOPIC = "ENRICHED_EVENTS";
 
 }
